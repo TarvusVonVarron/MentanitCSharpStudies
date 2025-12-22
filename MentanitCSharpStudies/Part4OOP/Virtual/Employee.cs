db4017d38242a21c19741877e81038dc77f86532
tarvusvarron97 <tarvusvarron97@gmail.com>
2025-12-22 22:32:10 +0300

    раздел виртуальных методов начал

---
+++ MentanitCSharpStudies/Part4OOP/Virtual/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part4OOP.Virtual
+{
+    public abstract class Employee
+    {
+        public virtual int Bonus { get; set; }
+        public abstract void GetSalary();
+    }
+}
---
+++ MentanitCSharpStudies/Part4OOP/Virtual/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part4OOP.Virtual
+{
+    public class Manager : Employee
+    {
+        public override int Bonus
+        {
+            get => base.Bonus;
+            set { if (value > 10 && value < 100) base.Bonus = value; }
+        }
+
+        public override void GetSalary()
+        {
+            Console.WriteLine("Manager's salary is $100");
+        }
+    }
+}
